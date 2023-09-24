2138e4148749f42732a99371adccef0f08a6134d
jawascript07 <striker-bold@live.com>
2023-09-24 21:24:27 +0500

    adding q1

---
+++ Q1_21L5787.java
@@ -0,0 +1,29 @@
+package Lab4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Q1_21L5787 {
+	
+	public static void main(String[] args) {
+		JFrame helloworld = new JFrame("Hello World");
+		helloworld.setSize(300, 100);
+		
+		JPanel contentPanel = new JPanel();
+		JButton clickme = new JButton("Click me");
+		
+		clickme.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent a) {
+				JOptionPane.showMessageDialog(helloworld, "Hello Swing");
+			}
+		});
+		
+		contentPanel.add(clickme);
+		helloworld.add(contentPanel);
+		
+		helloworld.setVisible(true);;
+	}
+}
